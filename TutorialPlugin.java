08e6d251f64135627efd82f4e99039d6c7336a90
fukuoka <hukuoka@S15.yeskist.net>
2016-11-08 20:30:53 +0900

    現在位置の3ブロック上に石をおく処理

--- TutorialPlugin.java
+++ TutorialPlugin.java
@@ -7,8 +7,6 @@ import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TutorialPlugin extends JavaPlugin {
@@ -22,24 +20,21 @@ public class TutorialPlugin extends JavaPlugin {
 	    public void onDisable() {
 	        getLogger().info("onDisableメソッドが呼び出されたよ！！");
 	    }
-	    @EventHandler
-	    public void onPlayerMove(PlayerMoveEvent evt) {
-	        // プレイヤーの位置を取得します。
-	        Location loc = evt.getPlayer().getLocation();
-	        // 位置のY座標を+3します。位置情報を変更しているだけで、実際にプレイヤーの位置が移動するわけではないことに注意してください。
-	        loc.setY(loc.getY() + 3);
-	        // 指定位置のブロックを取得します。
-	        Block b = loc.getBlock();
-	        // ブロックの種類に石（STONE）を設定します。
-	        b.setType(Material.STONE);
-	    }
 
 	    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 	        if (sender instanceof Player) {
 	            Player player = (Player) sender;
 	            if (cmd.getName().equalsIgnoreCase("upstone")) {
 	            	sender.sendMessage("ナイス!");
-	            	onPlayerMove(null);//←ここで上に石を置きたい
+	    	        // プレイヤーの位置を取得します。
+	    	        player.getLocation();
+	    	        Location loc=player.getLocation();
+	    	        // 位置のY座標を+3します。位置情報を変更しているだけで、実際にプレイヤーの位置が移動するわけではないことに注意してください。
+	    	        loc.setY(loc.getY() + 3);
+	    	        // 指定位置のブロックを取得します。
+	    	        Block b = loc.getBlock();
+	    	        // ブロックの種類に石（STONE）を設定します。
+	    	        b.setType(Material.STONE);
 
 	            }
 
