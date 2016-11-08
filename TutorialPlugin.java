7bea4750924b36ee3ad047a94fe44ff7aec255f3
fukuoka <hukuoka@S15.yeskist.net>
2016-11-08 19:17:43 +0900

    プレイヤーの上に石ブロックを出現させる処理

--- TutorialPlugin.java
+++ TutorialPlugin.java
@@ -1,38 +1,52 @@
 package com.github.fukuken1300.tutorialplugin;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TutorialPlugin extends JavaPlugin {
 
 	 @Override
 	    public void onEnable() {
-	        getLogger().info("onEnableメソッドが呼び出されたよ！！");
+	        getLogger().info("読み込まれました！");
 	    }
 
 	    @Override
 	    public void onDisable() {
 	        getLogger().info("onDisableメソッドが呼び出されたよ！！");
 	    }
+	    @EventHandler
+	    public void onPlayerMove(PlayerMoveEvent evt) {
+	        // プレイヤーの位置を取得します。
+	        Location loc = evt.getPlayer().getLocation();
+	        // 位置のY座標を+3します。位置情報を変更しているだけで、実際にプレイヤーの位置が移動するわけではないことに注意してください。
+	        loc.setY(loc.getY() + 3);
+	        // 指定位置のブロックを取得します。
+	        Block b = loc.getBlock();
+	        // ブロックの種類に石（STONE）を設定します。
+	        b.setType(Material.STONE);
+	    }
 
 	    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-	    	if (cmd.getName().equalsIgnoreCase("basic")) {
-	    		// プレイヤーが /basic を実行すると、この部分の処理が実行されます...
-	    		sender.sendMessage("成功");
-	    		return true;
-	    	} else if (cmd.getName().equalsIgnoreCase("basic2")) {
-	                    // プレイヤーが /basic2 を実行すると、この部分の処理が実行されます...
-	    		if (!(sender instanceof Player)) {
-	    			sender.sendMessage("このコマンドはゲーム内から実行してください。");
-	    		} else {
-	    			Player player = (Player) sender;
-	    			// コマンドの実行処理...
-	    		}
-	    		return true;
-	    	}
-	    	return false;
+	        if (sender instanceof Player) {
+	            Player player = (Player) sender;
+	            if (cmd.getName().equalsIgnoreCase("upstone")) {
+	            	sender.sendMessage("ナイス!");
+	            	onPlayerMove(null);//←ここで上に石を置きたい
+
+	            }
+
+	            return true;
+	        } else {
+	            sender.sendMessage(ChatColor.RED + "ゲーム内から実行してください！");
+	            return false;
+	        }
 	    }
 }
-
